7802f09e8dc4f560e21b884de78dc5c2e5a9a679
Aditya kumar Tiwari <sujanian3425@gmail.com>
2024-02-28 10:01:37 +0530

    Signup Form

---
+++ src/components/Form.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./form.css";
+export default function Form() {
+    const [data, setData] = useState({
+        user: "",
+        pass: "",
+        num: "",
+        mail: "",
+    });
+    function hanldeOutput() {
+        console.log(data);
+        let arr = [];
+        for (let i in data) {
+            arr.push(data[i]);
+        }
+        console.log(arr);
+        alert("Welcome " + data.user);
+    }
+    return (
+        <div className="Parent">
+            <h1>SignUp Form</h1>
+            <form onSubmit={hanldeOutput}>
+                <label htmlFor="UserName">Username : </label>
+                <input
+                    type="text"
+                    id="UserName"
+                    value={data.user}
+                    placeholder="  Enter Your User Name"
+                    onChange={(e) => setData({ ...data, user: e.target.value })}
+                />
+                <br />
+                <label htmlFor="Pass">Password : </label>
+                <input
+                    type="password"
+                    id="Pass"
+                    value={data.pass}
+                    placeholder="  Enter Your Password"
+                    onChange={(e) => setData({ ...data, pass: e.target.value })}
+                />
+                <br />
+                <label htmlFor="Num">Phone No : </label>
+                <input
+                    type="text"
+                    id="Num"
+                    placeholder="  Enter Your Number"
+                    value={data.num}
+                    onChange={(e) => setData({ ...data, num: e.target.value })}
+                />
+                <br />
+                <label htmlFor="mail" id="lmail">
+                    Email :
+                </label>
+                <input
+                    type="email"
+                    id="mail"
+                    placeholder="Enter Your Email address"
+                    value={data.mail}
+                    onChange={(e) => setData({ ...data, mail: e.target.value })}
+                    style={{ width: "200px" }}
+                />
+                <br />
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
